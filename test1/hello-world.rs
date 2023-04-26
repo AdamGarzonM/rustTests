4a5755731f89a1c1503e507ffae767121d8dce43
Adàm GMorales <73642190+AdamGarzonM@users.noreply.github.com>
2023-04-26 03:27:13 -0400

    hello world

---
+++ test1/hello-world.rs
@@ -0,0 +1,12 @@
+#[allow(unused_imports)]
+
+use std::env;
+
+fn printstart(message: &str){
+    println!("{}", message);
+}
+
+fn main() {
+    let hellomessage = "Hello Wrld";
+    printstart(hellomessage);
+}
